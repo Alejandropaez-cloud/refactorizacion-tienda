33656520f22ae8a7ceb6efee7b1f29470e7bb5a1
Alejandro <apaemil0312@g.educaand.es>
2026-02-09 09:27:20 +0100

    creamos el constructor

---
+++ refactorizaciontienda/src/main/java/refactorizaciontienda/Productos.java
@@ -0,0 +1,19 @@
+package refactorizaciontienda;
+
+public class Productos {
+    // no hay metodo main en esta clase ya que esta clase sera para hacer constructores,
+    // getters, setters y para eso no hace falta metodo main.
+    
+    // Creacion de los atributos.
+    private final String nombre;
+    private double precio;
+
+    // Constructor:
+    public Productos (String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    
+
+}
